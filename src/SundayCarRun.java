9870d70ec84265281ab9ca88d47fb465be7aeab6
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-06-27 15:12:11 +0530

    Wrote the code to check if car will able to run on sunday

---
+++ src/SundayCarRun.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SundayCarRun {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        sc.close();
+        long evenSum = 0, oddSum = 0;
+        while(num > 0){
+            long dig = num%10;
+            if(dig%2==0)
+              evenSum += dig;
+            else 
+              oddSum += dig;
+            num = num/10;
+        }
+        System.out.println((evenSum%4==0 || oddSum%3==0)?"Run on sunday":"Not run on sunday");
+    }
+}
