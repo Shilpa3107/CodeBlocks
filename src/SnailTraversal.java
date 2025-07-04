d95161287676c44e350f68f1504ec470a04c7c2d
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-07-04 12:23:12 +0530

    Wrote the code of snail traversal

---
+++ src/SnailTraversal.java
@@ -0,0 +1,21 @@
+public class SnailTraversal {
+    public static void main(String args[]){
+        int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,1,2},{3,4,5,6}};
+        for(int i = 0;i<arr[0].length;i++){
+            if(i%2==0){
+               int j = 0;
+               while(j<arr.length){
+                System.out.print(arr[j][i]+" ");
+                j++;
+               }
+            }
+            else{
+                int j = arr.length-1;
+                while(j>=0){
+                    System.out.print(arr[j][i]+" ");
+                    j--;
+                }
+            }
+        }
+    }
+}
