a5d3967d360ad5907fe28fb91b000e536bcf1f62
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-07-04 11:28:52 +0530

    Wrote the code of zigzag conversion

---
+++ src/ZigZagTraversal.java
@@ -0,0 +1,21 @@
+public class ZigZagTraversal {
+    public static void main(String args[]){
+        int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,1,2},{3,4,5,6}};
+        int j = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(i%2==0){
+              j = 0;
+              while(j<arr[i].length){
+                System.out.print(arr[i][j]+" ");
+                j++;
+            }
+            }
+            else {
+              j = arr[0].length;
+              while(j-->0){
+                System.out.print(arr[i][j]+" ");
+            }
+            }
+        }
+    }
+}
